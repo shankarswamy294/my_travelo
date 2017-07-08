ce31a565254dc1ebe12660982cbd93bc29d5244a
shankar_swamy <shankarswamy294@gmail.com>
2017-07-09 00:43:51 +0530

    settings changed

--- travelers/settings.py
+++ travelers/settings.py
@@ -155,3 +155,14 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'my_travelo/static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
