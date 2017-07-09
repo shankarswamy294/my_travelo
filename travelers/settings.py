307f1053c93ce95fe5df3d753a1ba2823f251b6f
shankar_swamy <shankarswamy294@gmail.com>
2017-07-09 14:46:24 +0530

    url change

--- travelers/settings.py
+++ travelers/settings.py
@@ -157,7 +157,7 @@ DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'mytravelo/static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
